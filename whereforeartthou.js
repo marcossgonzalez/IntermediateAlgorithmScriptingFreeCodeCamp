591a953b0ff43133ca1fcd75caf5d4d7faf7d80d
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-20 19:31:01 -0300

    Update lesson
    update lesson

--- whereforeartthou.js
+++ whereforeartthou.js
@@ -1,7 +1,12 @@
 //-------------------------------------------
 function whatIsInAName(collection, source) {
-
+    let array = Object.keys(source);
+    return collection.filter(object => array.every(key => object[key] === source[key]));
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, 
-{ first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 });
+//-------------------------------------------
