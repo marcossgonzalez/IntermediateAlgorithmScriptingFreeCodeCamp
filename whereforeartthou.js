ffffebda59afeb4432af49e9e90dbd786aeca942
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-20 19:31:18 -0300

    minor fix
    minor fix

--- whereforeartthou.js
+++ whereforeartthou.js
@@ -4,9 +4,5 @@ function whatIsInAName(collection, source) {
     return collection.filter(object => array.every(key => object[key] === source[key]));
 }
 
-
-
-
-
 whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 });
 //-------------------------------------------
