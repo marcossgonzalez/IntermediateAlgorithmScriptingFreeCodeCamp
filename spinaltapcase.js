0fa54b0a50b4a8836b03f8ba31827e65e10f9502
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-21 14:25:39 -0300

    Update lesson
    update lesson

--- spinaltapcase.js
+++ spinaltapcase.js
@@ -1,12 +1,14 @@
 //-------------------------------------------------
 function spinalCase(str) {
     let strCopy = str;
-    let regexSpace = /\w+/g;
+    let regexSpace = /\w+/;
     if (regexSpace.test(strCopy)) {
-        console.log(strCopy.split(/[^a-zA-Z]/g).join("-").toLowerCase());
-        return strCopy.split(/[^a-zA-Z]/g).join("-").toLowerCase();
+        strCopy = strCopy.split(/(?:_| )+/gi).join("-").replace(/([a-z])([A-Z])/g, "$1 $2").toLowerCase();
+        strCopy = strCopy.split(" ").join("-")
+        console.log(strCopy)
+        return strCopy.split(/(?:_| )+/gi).join("-").toLowerCase();
     }
     return str;
 }
 
-spinalCase("The_Andy_Griffith_Show")
+spinalCase("AllThe-small Things")
