ba7f9d9032adbecba17692925917ec4a4f5206f8
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-26 20:10:10 -0300

    Minor fix
    minor fix

--- dnapairing.js
+++ dnapairing.js
@@ -4,3 +4,4 @@ function pairElement(str) {
   }
   
   pairElement("GCG");
+//------------------------------------------------
--- searchandreplace.js
+++ searchandreplace.js
@@ -7,28 +7,28 @@ function myReplace(str, before, after) {
     let regexDown = /(?=.*[a-z])/;
     let checkIndex = copyStr.indexOf(beforeCopy);
     console.log(regexUpper.test(copyStr[checkIndex]));
-    if (regexUpper.test(copyStr[checkIndex])){
-      let indexAfter = afterCopy[afterCopy.indexOf(afterCopy)];
-      indexAfter = indexAfter.toUpperCase();
-      afterCopy = indexAfter.concat(afterCopy.substring(1));
-      console.log(indexAfter);
-      copyStr = copyStr.replace(beforeCopy, afterCopy);
-      console.log(copyStr);
+    if (regexUpper.test(copyStr[checkIndex])) {
+        let indexAfter = afterCopy[afterCopy.indexOf(afterCopy)];
+        indexAfter = indexAfter.toUpperCase();
+        afterCopy = indexAfter.concat(afterCopy.substring(1));
+        console.log(indexAfter);
+        copyStr = copyStr.replace(beforeCopy, afterCopy);
+        console.log(copyStr);
     }
-    if (regexDown.test(copyStr[checkIndex])){
-      let indexAfter = afterCopy[afterCopy.indexOf(afterCopy)];
-      indexAfter = indexAfter.toLowerCase();
-      afterCopy = indexAfter.concat(afterCopy.substring(1));
-      console.log(indexAfter);
-      copyStr = copyStr.replace(beforeCopy, afterCopy);
-      console.log(copyStr);
+    if (regexDown.test(copyStr[checkIndex])) {
+        let indexAfter = afterCopy[afterCopy.indexOf(afterCopy)];
+        indexAfter = indexAfter.toLowerCase();
+        afterCopy = indexAfter.concat(afterCopy.substring(1));
+        console.log(indexAfter);
+        copyStr = copyStr.replace(beforeCopy, afterCopy);
+        console.log(copyStr);
     }
     copyStr = copyStr.replace(before, after);
     console.log(copyStr);
-    
-  
+
+
     return copyStr;
-  }
-  
-  myReplace("I think we should look up there", "up", "Down")
+}
+
+myReplace("I think we should look up there", "up", "Down")
 //------------------------------------------------
