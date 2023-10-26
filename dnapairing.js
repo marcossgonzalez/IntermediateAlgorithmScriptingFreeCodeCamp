17ea01e6b5eb465a5cb96e9e5283a42a9b8dc4c3
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-26 20:22:12 -0300

    Update Lesson
    Update lesson

--- dnapairing.js
+++ dnapairing.js
@@ -1,7 +1,23 @@
 //------------------------------------------------
 function pairElement(str) {
-    return str;
-  }
-  
-  pairElement("GCG");
+    let array = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "C") {
+            array.push(["C", "G"]);
+        }
+        else if (str[i] == "G") {
+            array.push(["G", "C"]);
+        }
+        if (str[i] == "A") {
+            array.push(["A", "T"]);
+        }
+        else if (str[i] == "T") {
+            array.push(["T", "A"]);
+        }
+    }
+    console.log(array);
+    return array;
+}
+
+pairElement("CTCTA");
 //------------------------------------------------
