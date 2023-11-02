54737e6bc12e5815388f187c4e1b6038f77fa95d
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-02 02:16:31 -0300

    minor fix
    minor fix of indentation

--- dropit.js
+++ dropit.js
@@ -3,17 +3,18 @@ function dropElements(arr, func) {
     let array = arr;
     let result = func;
     let arrayFinal = [];
-    for (let i = 0; i<array.length; i++){
-      if(result(array[i])){
-        while(i<array.length){
-        arrayFinal.push(array[i]);
-        i++;}
-        
-      }
+    for (let i = 0; i < array.length; i++) {
+        if (result(array[i])) {
+            while (i < array.length) {
+                arrayFinal.push(array[i]);
+                i++;
+            }
+
+        }
     }
     console.log(array);
-  
+
     return arrayFinal;
-  }
-  
-  dropElements([0, 1, 0, 1], function(n) {return n === 1;})
+}
+
+dropElements([0, 1, 0, 1], function (n) { return n === 1; })
