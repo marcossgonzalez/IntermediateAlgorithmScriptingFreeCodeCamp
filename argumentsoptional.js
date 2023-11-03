44126399591d361bebfe8058f29d92a6c0197cf4
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-03 01:03:13 -0300

    Update lesson
    update lesson

---
+++ argumentsoptional.js
@@ -0,0 +1,24 @@
+//--------------------------------------------
+function addTogether() {
+    const [a, b] = arguments;
+    if (typeof (a) !== "number") {
+        return undefined;
+    } else if (arguments.length === 1) {
+        function add(b) {
+            if (typeof (b) !== "number") {
+                return undefined;
+            }
+            else { return a + b; }
+        }
+        return add;
+    }
+    else if (typeof b !== "number") {
+        return undefined;
+
+    } else {
+        return a + b;
+    }
+
+}
+
+addTogether(2, 3)
