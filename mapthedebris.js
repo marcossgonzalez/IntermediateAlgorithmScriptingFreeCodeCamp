846b8f3be3147d4f5fa49c12f9a9e8558015b5a7
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-03 01:58:50 -0300

    Update lesson
    update ongoing lesson

---
+++ mapthedebris.js
@@ -0,0 +1,8 @@
+//-----------------------------------------------
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    return arr;
+}
+
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
